c4533eb7bea3a27f8cd906d387dfd2707f2ec746
Iktae Kim <kimit0310@gmail.com>
2026-04-13 09:24:13 -0400

    update audio device name in configuration and adjust checkbox state in GUI

--- gui.py
+++ gui.py
@@ -129,6 +129,7 @@ class RecorderApp(tk.Tk):
                 checkbox = ttk.Checkbutton(
                     self.audio_devices_frame, text=device_key, variable=var
                 )
+                checkbox.state(["!alternate"])
                 checkbox.grid(row=i, column=0, sticky=tk.W, pady=2)
             self.log_message("GUI Log: Audio devices refreshed")
         else:
